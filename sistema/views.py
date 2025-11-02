2346dee22bd9a366bdd3f6a59efad9cb514dfcbe
Kevin Guzmán <guzmankevin025@gmail.com>
2025-11-02 12:29:10 -0600

    Actualización graficos

--- sistema/views.py
+++ sistema/views.py
@@ -1,6 +1,27 @@
+import psutil
+import platform
 from django.shortcuts import render
-from .utils import obtener_datos_sistema
 
-def home(request):
+def obtener_datos_sistema():
+    cpu = psutil.cpu_percent()
+    ram = psutil.virtual_memory()
+    disco = psutil.disk_usage('/')
+    
+    datos = {
+        'cpu': cpu,
+        'cpu_libre': 100 - cpu,
+        'ram_total': round(ram.total / (1024**3), 2),
+        'ram_usada': round(ram.used / (1024**3), 2),
+        'ram_percent': ram.percent,
+        'ram_libre': round(ram.total / (1024**3) - ram.used / (1024**3), 2),
+        'disco_total': round(disco.total / (1024**3), 2),
+        'disco_usado': round(disco.used / (1024**3), 2),
+        'disco_libre': round(disco.free / (1024**3), 2),
+        'os_info': platform.platform(),
+        'cores': psutil.cpu_count(),
+    }
+    return datos
+
+def index(request):
     datos = obtener_datos_sistema()
-    return render(request, "sistema/index.html", {"datos": datos})
+    return render(request, 'sistema/index.html', {'datos': datos})
